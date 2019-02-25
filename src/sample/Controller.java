1d37ebcf62c77c9fd1cf482d0b5f14bf57cc5161
Sadra Hallaj <45514009+sadrahallaj@users.noreply.github.com>
2019-02-26 01:41:10 +0330

    Update Controller.java

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,8 +1,21 @@
 package sample;
 
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Arc;
+import javafx.scene.shape.ArcType;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.StrokeType;
+import javafx.scene.text.Text;
+import javafx.stage.StageStyle;
+
+import java.util.Optional;
 
 public class Controller {
 
@@ -16,11 +29,22 @@ public class Controller {
     private boolean waitingForPlacement = false;
 
     public void btnNewNodeClicked() {
-        setMouseListener();
+        waitingForPlacement = true;
+        int value;
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("creating new node");
+        dialog.setHeaderText("Creating a new Node");
+        dialog.setContentText("Please enter the value for this new node:");
+        Optional<String> result = dialog.showAndWait();
+        if (result.isPresent()) {
+            value = Integer.parseInt(result.get());
+            setPaneListener(value);
+        }
+
+
     }
 
     public void newLineclicked() {
-
     }
 
     public void Finishclicked() {
@@ -32,22 +56,42 @@ public class Controller {
     }
 
     public void bfsclicked() {
-
     }
 
     public void dfsclicked() {
 
     }
 
-    public void setMouseListener() {
+    public void setPaneListener(int value) {
         customPane.setOnMouseClicked(event -> {
+            if (waitingForPlacement) {
 
-//            Button button = new Button();
-//            button.setLayoutX(event.getX());
-//            button.setLayoutY(event.getY());
-//            button.setText("Hi");
-//
-//            customPane.getChildren().add(button);
+                double centerX = event.getX();
+                double centerY = event.getY();
+
+                createCiecle(centerX , centerY , value);
+                waitingForPlacement = false;
+
+            }
         });
+    }
+
+    // drawing Ring :
+    public void createCiecle(double centerX , double centerY , int value){
+        // creating ring and add it to custom pane :
+        Circle circleBig = new Circle(centerX, centerY, 25);
+        Circle circleSmall = new Circle(centerX, centerY, 20);
+        circleBig.setFill(javafx.scene.paint.Color.BLUE);
+        circleSmall.setFill(javafx.scene.paint.Color.WHITE);
+
+        // add text to the circle :
+        Text text   = new Text(String.valueOf(value));
+        text.setX(centerX - 3);
+        text.setY(centerY + 2);
+
+        //add circle and its value to custom pane
+        customPane.getChildren().add(circleBig);
+        customPane.getChildren().add(circleSmall);
+        customPane.getChildren().add(text);
     }
 }
