563fc6fe53715b7abb7642a87239d0de85f0848b
theFidelio <47862120+theFidelio@users.noreply.github.com>
2019-02-27 16:11:24 +0330

    title changed

--- src/sample/Node.java
+++ src/sample/Node.java
@@ -1,13 +1,39 @@
 package sample;
 
-public class Node {
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+
+public class Node extends Button {
     private int index;
     private boolean isVisited;
+    private double centerX;
+    private double centerY;
+    private String btnText ;
+    private Color btnColor ;
+    public double getCenterX() {
+        return centerX;
+    }
 
-    public Node(int index){
+    public void setCenterX(double centerX) {
+        this.centerX = centerX;
+    }
+
+    public double getCenterY() {
+        return centerY;
+    }
+
+    public void setCenterY(double centerY) {
+        this.centerY = centerY;
+    }
+
+    public Node(int index, double centerX , double centerY){
         this.isVisited = false;
         this.index = index;
+        this.centerX = centerX ;
+        this.centerY = centerY ;
+        this.btnText = String.valueOf(index);
     }
+
 
     public int getIndex() {
         return index;
