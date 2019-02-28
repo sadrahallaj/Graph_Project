d4defa26e7d98cd360433ff79f8506bac5930ce2
Sadra Hallaj <45514009+sadrahallaj@users.noreply.github.com>
2019-02-28 17:49:31 +0330

    Update Node.java

--- src/sample/Node.java
+++ src/sample/Node.java
@@ -6,32 +6,16 @@ import javafx.scene.paint.Color;
 public class Node extends Button {
     private int index;
     private boolean isVisited;
-    private double centerX;
-    private double centerY;
-    private String btnText ;
-    private Color btnColor ;
-    public double getCenterX() {
-        return centerX;
-    }
 
-    public void setCenterX(double centerX) {
-        this.centerX = centerX;
-    }
-
-    public double getCenterY() {
-        return centerY;
-    }
-
-    public void setCenterY(double centerY) {
-        this.centerY = centerY;
-    }
 
     public Node(int index, double centerX , double centerY){
+        super();
         this.isVisited = false;
         this.index = index;
-        this.centerX = centerX ;
-        this.centerY = centerY ;
-        this.btnText = String.valueOf(index);
+        this.setText(String.valueOf(index));
+        this.setLayoutX(centerX);
+        this.setLayoutY(centerY);
+        this.setStyle("-fx-background-radius: 50 ;");
     }
 
 
@@ -50,5 +34,5 @@ public class Node extends Button {
     public void setVisited(boolean visited) {
         isVisited = visited;
     }
-
 }
+
