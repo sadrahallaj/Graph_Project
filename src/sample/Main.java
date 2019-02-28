53b20f263b98d43463fd9d5350607cf6d0e46c12
Sadra Hallaj <sadra.hallajhassas@gmail.com>
2019-02-28 18:26:33 +0330

    delete dialog

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,6 +13,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Breadth First Search or Depth First Search for a Graph");
+        Image image = new Image(getClass().getResourceAsStream("graph.png"));
+        primaryStage.getIcons().add(image);
         primaryStage.setScene(new Scene(root, 900, 850));
         primaryStage.show();
     }
