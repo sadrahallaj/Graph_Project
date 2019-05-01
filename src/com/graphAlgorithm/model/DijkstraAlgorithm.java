dd34a288c4cf320dbb8d7b1773bb613e94d68ba9
sadrahallaj <sadra.hallajhassas@gmail.com>
2019-05-02 03:55:48 +0430

    fix bug in algorithm

--- src/com/graphAlgorithm/model/DijkstraAlgorithm.java
+++ src/com/graphAlgorithm/model/DijkstraAlgorithm.java
@@ -3,12 +3,12 @@ package com.graphAlgorithm.model;
 import com.graphAlgorithm.view.other.Pair;
 import java.util.LinkedList;
 
-class DijkstraAlgorithm {
+public class DijkstraAlgorithm {
 
     private LinkedList<Pair<Integer,Integer>> data = new LinkedList<>();
     private int source;
 
-    LinkedList<Pair<Integer,Integer>> algorithm(LinkedList<LinkedList<Pair<Integer,Integer>>> adjList, int source){
+    public LinkedList<Pair<Integer,Integer>> algorithm(LinkedList<LinkedList<Pair<Integer,Integer>>> adjList, int source){
         this.source = source;
 
         int s = source ,visNum = 0;
@@ -28,8 +28,8 @@ class DijkstraAlgorithm {
             for (int i=0; i<adjList.get(s).size(); i++){
                 Pair<Integer,Integer> now = adjList.get(s).get(i);
                 //update data table
-                if (dist[now.first] > now.second){
-                    dist[now.first] = now.second;
+                if (dist[now.first] > now.second + dist[s]){
+                    dist[now.first] = now.second + dist[s];
                     prev[now.first] = s;
                 }
             }
@@ -57,14 +57,14 @@ class DijkstraAlgorithm {
         return data;
     }
 
-    LinkedList<Integer> shortestPath(int d){
+    public LinkedList<Integer> shortestPath(int d){
         int currentVertex = d ;
         LinkedList<Integer> path = new LinkedList<>();
         path.add(d);
         while (currentVertex != source){
-           if(data.get(currentVertex).getSecond() == -1 ) break;
-           currentVertex = data.get(currentVertex).getSecond();
-           path.add(currentVertex);
+            if(data.get(currentVertex).getSecond() == -1 ) break;
+            currentVertex = data.get(currentVertex).getSecond();
+            path.add(currentVertex);
         }
         return reverseLinkedList(path);
     }
