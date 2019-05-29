4ebecb8209448301fe80ab11b81d92fb4a21bd9b
theFidelio <alikarbasi78@gmail.com>
2019-05-29 20:12:32 +0430

    save adjMatrix v0.1

--- src/com/graphAlgorithm/view/main/MainPage.java
+++ src/com/graphAlgorithm/view/main/MainPage.java
@@ -247,6 +247,18 @@ public class MainPage {
     }
 
     @FXML
+    private void saveAdjMatrix(){
+        int[][] adjMatrix = new int[adjList.size()][adjList.size()];
+        for (int i = 0; i<adjList.size();i++){
+            for (int j = 0; j<adjList.get(i).size();j++) {
+                adjMatrix[i][adjList.get(i).get(j).getFirst()] = adjList.get(i).get(j).getSecond();
+            }
+
+        }
+
+    }
+
+    @FXML
     private void restartHandler() {
         waitingForPlacement = false;
         btnFinish.setDisable(true);
