868bcb67e747802520da74e7a728914d228a4d4f
sadrahallaj <sadra.hallajhassas@gmail.com>
2019-07-06 16:44:57 +0430

    Modify - aco tcp

--- src/com/graphAlgorithm/model/AcoTsp.java
+++ src/com/graphAlgorithm/model/AcoTsp.java
@@ -3,23 +3,24 @@ package com.graphAlgorithm.model;
 import java.util.LinkedList;
 
 public class AcoTsp {
-    private double[][] pheromoneMatrix ;
-    private double[][] distancesMatrix ;
-    private double[][] visibilityOfEdgeMatrix ;
+    private double[][] pheromoneMatrix;
+    private double[][] distancesMatrix;
+    private double[][] visibilityOfEdgeMatrix;
     private int source;
-    private double[][] pathForEachAnt ;
-    private double [] pathCostForEachAnt ;
-    
-    public AcoTsp(int source , double[][] distanceMatrix){
-        this.source = source ;
-        this.distancesMatrix = distanceMatrix ;
+    private double[][] pathForEachAnt;
+    private double[] pathCostForEachAnt;
+
+    public AcoTsp(int source, double[][] distanceMatrix) {
+        this.source = source;
+        this.distancesMatrix = distanceMatrix;
 
         // initial pheromoneMatrix : 
         // for all edges pheromone set to 1 at first 
         pheromoneMatrix = new double[distancesMatrix.length][distancesMatrix.length];
         for (int i = 0; i < distancesMatrix.length; i++) {
             for (int j = 0; j < distanceMatrix.length; j++) {
-                pheromoneMatrix[i][j] = 1 ;
+                if (i == j) pheromoneMatrix[i][j] = 0;
+                else pheromoneMatrix[i][j] = 1;
             }
         }
 
@@ -28,17 +29,17 @@ public class AcoTsp {
         visibilityOfEdgeMatrix = new double[distancesMatrix.length][distancesMatrix.length];
         for (int i = 0; i < distancesMatrix.length; i++) {
             for (int j = 0; j < distancesMatrix.length; j++) {
-                if(distancesMatrix[i][j] == 0 ) visibilityOfEdgeMatrix[i][j] = 0 ;
+                if (distancesMatrix[i][j] == 0) visibilityOfEdgeMatrix[i][j] = 0;
                 else visibilityOfEdgeMatrix[i][j] = 1 / distancesMatrix[i][j];
             }
         }
 
         // initial pathForEachAnt :
         // for all ants set to 0 at first
-        pathForEachAnt = new double[distancesMatrix.length][distancesMatrix.length + 1 ];
-        for (int i = 0; i < distancesMatrix.length ; i++) {
-            for (int j = 0; j < distancesMatrix.length +1  ; j++) {
-                pathForEachAnt[i][j] = i ;
+        pathForEachAnt = new double[distancesMatrix.length][distancesMatrix.length + 1];
+        for (int i = 0; i < distancesMatrix.length; i++) {
+            for (int j = 0; j < distancesMatrix.length + 1; j++) {
+                pathForEachAnt[i][j] = i;
             }
         }
 
@@ -46,31 +47,122 @@ public class AcoTsp {
         // for all ants set to 0 at first
         pathCostForEachAnt = new double[distancesMatrix.length];
         for (int i = 0; i < pathCostForEachAnt.length; i++) {
-            pathCostForEachAnt[i] = 0 ;
+            pathCostForEachAnt[i] = 0;
         }
     }
 
-    private void algorithm(){
+    private void algorithm() {
         // here should update these things :
         // 1 - pheromoneMatrix
         // 2 - pathForEachAnt
         // 3 - pathCostForEachAnt
-    }
 
+        // Iteration loop (main loop)
+        for (int i = 0; i < distancesMatrix.length; i++) {
+            // loop for each ant :
+            for (int j = 0; j < distancesMatrix.length; j++) {
+                // loop for each edge :
+                boolean[] visited = new boolean[distancesMatrix.length];
+                for (int k = 0; k < visited.length; k++) {
+                    visited[k] = false;
+                }
+                visited[j] = true;
+                int currentVertex = j;
+                int counter = 1 ;
+                while (countUnvisitedVertexies(visited) != 0) {
+                    int nextVertex = findNextVertex(currentVertex, visited);
+                    pathForEachAnt[j][counter++] = nextVertex ;
+                    currentVertex = nextVertex ;
+                }
+                pathForEachAnt[j][counter] = j ;
+            }
 
-    private int totalCostOfList(LinkedList<Integer> antList){
-        int cost = 0 ;
-        int currentVertex =  antList.getFirst() ;
-        for (int i = 1; i < antList.size() ; i++) {
-            int nextVertex = antList.get(i);
-            cost += distancesMatrix[currentVertex][nextVertex];
+            //update pheromoneMatrix :
+            for (int j = 0; j < pheromoneMatrix.length; j++) {
+                for (int k = 0; k < pheromoneMatrix.length; k++) {
+                    // update pheromone for edge j , k :
+                    // we should know if antk has travel throw j , k ?
+
+                }
+            }
+
         }
-        return cost ;
     }
 
-    private double[] getResult(){
+
+   // Lk
+    private int totalCostOfArray(int[] array) {
+        int cost = 0;
+        int currentVertex = array[0];
+        for (int i = 1; i < array.length; i++) {
+            int nextVertex = array[i];
+            cost += distancesMatrix[currentVertex][nextVertex];
+            currentVertex = nextVertex;
+        }
+        return cost;
+    }
+
+    private double[] getResult() {
         return pathForEachAnt[this.source];
     }
 
+    private int countUnvisitedVertexies(boolean[] visitedArray) {
+        int count = 0;
+        for (int i = 0; i < visitedArray.length; i++) {
+            if (visitedArray[i] == false) count++;
+        }
+        return count;
+    }
 
+    private int findNextVertex(int j, boolean[] visited) {
+        LinkedList<Integer> vertexies = new LinkedList<>();
+        for (int i = 0; i < distancesMatrix.length; i++) {
+            if (distancesMatrix[j][i] != 0 && visited[i] != false) vertexies.add(i);
+        }
+
+        // ∑Ʈα (1/n) β : maxraj loop :
+        double maxraj = 0;
+        for (int k = 0; k < distancesMatrix[j].length; k++) {
+            double taw = pheromoneMatrix[j][vertexies.get(k)];
+            if (pheromoneMatrix[j][k] != 0)
+                maxraj += taw * visibilityOfEdgeMatrix[j][vertexies.get(k)];
+        }
+
+        LinkedList<Double> probiibltyOfEachEdge = new LinkedList<>();
+
+        // Ʈα (1/n) β / ∑Ʈα (1/n) β
+
+        for (int k = 0; k < vertexies.size(); k++) {
+            int nextVertex = vertexies.get(k);
+            double sorat = pheromoneMatrix[j][nextVertex] * visibilityOfEdgeMatrix[j][nextVertex];
+            probiibltyOfEachEdge.add(sorat / maxraj);
+        }
+
+        return vertexies.get(findMaxInInLinkedList(probiibltyOfEachEdge));
+    }
+
+    private int findMaxInInLinkedList(LinkedList<Double> list){
+        double max = Double.MIN_VALUE ;
+        int indexOfMaxValue = 0 ;
+        for (int i = 0; i < list.size(); i++) {
+            if(list.get(i) > max ){
+                max = list.get(i);
+                indexOfMaxValue = i ;
+            }
+        }
+
+        return indexOfMaxValue ;
+    }
+
+    private boolean isAntHasGoneThrowEdge(int j , int k , int ant){
+
+        boolean condition = false ;
+        for (int i = 0; i < pathForEachAnt[ant].length; i++) {
+            if(pathForEachAnt[ant][i] == j && (((i + 1 <=pathForEachAnt[ant].length)&& pathForEachAnt[ant][i+1] == k ) || ((i - 1 >= 0 ) && pathForEachAnt[ant][i - 1 ] == k ) )
+               || pathForEachAnt[ant][i] == k && (((i + 1 <=pathForEachAnt[ant].length)&& pathForEachAnt[ant][i+1] == j ) || ((i - 1 >= 0 ) && pathForEachAnt[ant][i - 1 ] == j ))){
+                   condition = true ;
+            }
+        }
+        return condition ;
+    }
 }
